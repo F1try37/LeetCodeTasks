a6ea5d827fa9a4eea4ccd566838209d16ae86448
F1try37 <bildb20@yandex.ru>
2026-07-22 00:54:44 +0300

    Add task 20 solution

---
+++ src/main/java/Tasks/ValidParentheses20.java
@@ -0,0 +1,53 @@
+package Tasks;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidParentheses20 {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        char[] chars = s.toCharArray();
+        for (char ch: chars) {
+            if (ch == '[') {
+                stack.push(ch);
+            } else if (!stack.isEmpty() && ch == ']') {
+                if (stack.peek() != '[') return false;
+                else stack.pop();
+            } else if (ch == '{') {
+                stack.push(ch);
+            } else if (!stack.isEmpty() && ch == '}') {
+                if (stack.peek() != '{') return false;
+                else stack.pop();
+            } else if (ch == '(') {
+                stack.push(ch);
+            } else if (!stack.isEmpty() && ch == ')') {
+                if (stack.peek() != '(') return false;
+                else stack.pop();
+            } else return false;
+        }
+        return stack.isEmpty();
+    }
+
+    public boolean isValidMap(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        char[] chars = s.toCharArray();
+        Map<Character,Character> pairs = Map.of(')','(','}','{',']','[');
+        for (char ch: chars) {
+            if (pairs.containsValue(ch)) {
+                stack.push(ch);
+            } else if (!stack.isEmpty() && pairs.containsKey(ch)) {
+                if (stack.peek() == pairs.get(ch)) stack.pop();
+                else return false;
+            } else return false;
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        ValidParentheses20 solution = new ValidParentheses20();
+        String s = "([])";
+        System.out.println(solution.isValidMap(s));
+    }
+}
